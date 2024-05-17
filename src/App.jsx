fcb2e92485e63518a1c622c48a4319434c92fcb5
pandawaa <pallakangoding@gmail.com>
2024-05-17 16:04:23 +0800

    add Pricing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Services from "./components/Services";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
       </div>
     </>
   );
