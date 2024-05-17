d508470f4181d85ca3cfce2cbbaef20736bda736
pandawaa <pallakangoding@gmail.com>
2024-05-17 14:43:42 +0800

    add Benefits Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -7,6 +8,7 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
     </>
   );
