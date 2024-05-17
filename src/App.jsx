8bd32dec58d6d5de640740ceaaea93f849fb71c2
pandawaa <pallakangoding@gmail.com>
2024-05-17 11:41:39 +0800

    add Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
     </>
   );
