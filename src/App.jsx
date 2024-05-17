d348e66637b99bffd119121173732c7e681b3d7c
pandawaa <pallakangoding@gmail.com>
2024-05-17 15:11:31 +0800

    add Collaboration component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -9,6 +10,7 @@ function App() {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
     </>
   );
